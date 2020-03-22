d2dc89b82e069e1fad45546b4ee3279fa838ae81
manadhi <udhi.permana@gmail.com>
2020-03-22 14:25:58 +0700

    update main activity and link login to register page

--- app/src/main/java/com/udhipe/simpleapplication/login/LoginActivity.java
+++ app/src/main/java/com/udhipe/simpleapplication/login/LoginActivity.java
@@ -2,15 +2,30 @@ package com.udhipe.simpleapplication.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.button.MaterialButton;
 import com.udhipe.simpleapplication.R;
+import com.udhipe.simpleapplication.register.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    MaterialButton btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
