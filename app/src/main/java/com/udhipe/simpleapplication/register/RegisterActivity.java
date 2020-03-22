c921c85b5f9665361c77c2f6706566bf9a49a723
manadhi <udhi.permana@gmail.com>
2020-03-22 14:30:23 +0700

    link register page to login page

--- app/src/main/java/com/udhipe/simpleapplication/register/RegisterActivity.java
+++ app/src/main/java/com/udhipe/simpleapplication/register/RegisterActivity.java
@@ -2,15 +2,30 @@ package com.udhipe.simpleapplication.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.button.MaterialButton;
 import com.udhipe.simpleapplication.R;
+import com.udhipe.simpleapplication.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    MaterialButton btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
